bdff749863c5b9803e5f16cafbc25eda430fec65
ihourglass <inversionhourglass@outlook.com>
2024-09-21 16:43:52 +0800

    Move the method InspectType into a separate file.

--- src/Pooling.Fody/ModuleWeaver.cs
+++ src/Pooling.Fody/ModuleWeaver.cs
@@ -50,69 +50,6 @@ namespace Pooling.Fody
             }
         }
 
-        private void InspectType(TypeDefinition typeDef)
-        {
-            MethodMatcher[]? inclusiveMethods = null;
-            MethodMatcher[]? exclusiveMethods = null;
-
-            if (_config.InclusiveMethods.Length > 0)
-            {
-                inclusiveMethods = MatchType(_config.InclusiveMethods, typeDef);
-                if (inclusiveMethods.Length == 0) return;
-            }
-            if (_config.ExclusiveMethods.Length > 0)
-            {
-                exclusiveMethods = MatchType(_config.ExclusiveMethods, typeDef);
-            }
-
-            foreach (var methodDef in typeDef.Methods)
-            {
-                var signature = SignatureParser.ParseMethod(methodDef, _config.CompositeAccessibility);
-                var exclusivePattern = exclusiveMethods?.FirstOrDefault(x => x.IsMatch(signature)).Pattern;
-                if (exclusivePattern != null)
-                {
-                    WriteInfo($"{methodDef} is excluded by the exclusive-methods pattern ({exclusivePattern}).");
-                    continue;
-                }
-
-                if (inclusiveMethods != null)
-                {
-                    var inclusivePattern = inclusiveMethods.FirstOrDefault(x => x.IsMatch(signature)).Pattern;
-                    if (inclusivePattern == null) continue;
-                    WriteInfo($"{methodDef} is included by the inclusive-methods pattern ({inclusivePattern}).");
-                }
-                else
-                {
-                    WriteInfo($"{methodDef} is included because the inclusive-methods configuration is absent.");
-                }
-
-                InspectMethod(methodDef);
-            }
-
-            if (typeDef.HasNestedTypes)
-            {
-                foreach (var nestedTypeDef in typeDef.NestedTypes)
-                {
-                    InspectType(nestedTypeDef);
-                }
-            }
-
-            static MethodMatcher[] MatchType(MethodMatcher[] matchers, TypeDefinition typeDef)
-            {
-                var matchedMatcher = new List<MethodMatcher>();
-                var typeSignature = SignatureParser.ParseType(typeDef);
-                foreach (var matcher in matchers)
-                {
-                    if (matcher.DeclaringTypeMatcher.IsMatch(typeSignature))
-                    {
-                        matchedMatcher.Add(matcher);
-                    }
-                }
-
-                return matchedMatcher.ToArray();
-            }
-        }
-
         private void InspectMethod(MethodDefinition methodDef)
         {
             var instructions = methodDef.Body.Instructions;
