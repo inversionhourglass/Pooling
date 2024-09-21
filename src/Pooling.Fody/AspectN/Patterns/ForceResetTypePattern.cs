5345afb4b0aa500308eceb741e29365e9070b78a
ihourglass <inversionhourglass@outlook.com>
2024-09-21 14:56:42 +0800

    Add ForceResetTypePrefixParser.

---
+++ src/Pooling.Fody/AspectN/Patterns/ForceResetTypePattern.cs
@@ -0,0 +1,35 @@
+﻿using Cecil.AspectN;
+using Cecil.AspectN.Patterns;
+using System.Collections.Generic;
+
+namespace Pooling.Fody.AspectN.Patterns
+{
+    internal class ForceResetTypePattern(IIntermediateTypePattern pattern) : IIntermediateTypePattern
+    {
+        public bool IsAny => pattern.IsAny;
+
+        public bool IsVoid => pattern.IsVoid;
+
+        public bool AssignableMatch => pattern.AssignableMatch;
+
+        public void Compile(List<GenericParameterTypePattern> genericParameters, bool genericIn)
+        {
+            pattern.Compile(genericParameters, genericIn);
+        }
+
+        public bool IsMatch(TypeSignature signature)
+        {
+            return pattern.IsMatch(signature);
+        }
+
+        public GenericNamePattern SeparateOutMethod()
+        {
+            return pattern.SeparateOutMethod();
+        }
+
+        public DeclaringTypeMethodPattern ToDeclaringTypeMethod(params string[] methodImplicitPrefixes)
+        {
+            return pattern.ToDeclaringTypeMethod(methodImplicitPrefixes);
+        }
+    }
+}
---
+++ src/Pooling.Fody/AspectN/Patterns/Parsers/ForceResetTypePrefixParser.cs
@@ -0,0 +1,16 @@
+﻿using Cecil.AspectN.Patterns;
+using Cecil.AspectN.Patterns.Parsers;
+using Cecil.AspectN.Tokens;
+
+namespace Pooling.Fody.AspectN.Patterns.Parsers
+{
+    internal class ForceResetTypePrefixParser : ITypePrefixParser
+    {
+        public bool IsMatch(Token token) => token.Value == '~';
+
+        public IIntermediateTypePattern ParseType(TokenSource tokens)
+        {
+            return new ForceResetTypePattern(Parser.ParseType(tokens));
+        }
+    }
+}
