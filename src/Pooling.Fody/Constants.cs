5df3f53c36577b44d593788d35b7a79396536170
ihourglass <inversionhourglass@outlook.com>
2024-09-22 17:40:42 +0800

    More constant.

--- src/Pooling.Fody/Constants.cs
+++ src/Pooling.Fody/Constants.cs
@@ -4,6 +4,10 @@
     {
         public const string TYPE_Pool_1 = "Pooling.Pool`1";
         public const string TYPE_IPoolItem = "Pooling.IPoolItem";
+        public const string TYPE_NonPooledAttribute = "Pooling.NonPooledAttribute";
+
+        public const string PROP_Types = "Types";
+        public const string PROP_Pattern = "Pattern";
 
         public const string METHOD_Get = "Get";
         public const string METHOD_Return = "Return";
