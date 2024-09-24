cdd461899d83ea66bc6a22dc7fc9219b14c1fabd
ihourglass <inversionhourglass@outlook.com>
2024-09-25 02:32:37 +0800

    Add method CallResetMethod to PoolItem.

--- src/Pooling.Fody/PoolItem.cs
+++ src/Pooling.Fody/PoolItem.cs
@@ -1,5 +1,7 @@
-﻿using Mono.Cecil;
+﻿using Fody;
+using Mono.Cecil;
 using Mono.Cecil.Cil;
+using System.Collections.Generic;
 
 namespace Pooling.Fody
 {
@@ -15,5 +17,21 @@ namespace Pooling.Fody
         public TypeReference ItemTypeRef { get; } = itemTypeRef;
 
         public MethodDefinition? ResetMethodDef { get; } = resetMethodDef;
+
+        public Instruction[] CallResetMethod(BaseModuleWeaver moduleWeaver, TypeReference[] genericArguments)
+        {
+            if (ResetMethodDef == null) return [];
+
+            List<Instruction> calling = [
+                Instruction.Clone(),
+                moduleWeaver.Import(ResetMethodDef).WithGenerics(genericArguments).CallAny()
+            ];
+            if (!ResetMethodDef.ReturnType.IsVoid())
+            {
+                calling.Add(Instruction.Create(OpCodes.Pop));
+            }
+
+            return calling.ToArray();
+        }
     }
 }
