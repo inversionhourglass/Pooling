e0c912026763e2cbdd751aa6433814a50124f893
ihourglass <inversionhourglass@outlook.com>
2024-10-14 03:17:42 +0800

    Add an MSIL inspection test case.

---
+++ test/TestAssemblies/SingleFeatureCases/Cases/ILInspects/GenericTypeMethod.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SingleFeatureCases.Cases.ILInspects
+{
+    internal class GenericTypeMethod<T>
+    {
+        private async ValueTask<object?> LastInner<TT>(List<T> list1, List<TT> list2)
+        {
+            await Task.Yield();
+            return list1.Count > list2.Count ? list1.Last() : list2.Last();
+        }
+    }
+}
